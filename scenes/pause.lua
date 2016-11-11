3ae9a3b0a5f4c9849cfe7d64fb38be8a90b2f9a6
Jonas Pohren <jonaspohren@gmail.com>
2016-11-11 14:13:02 -0200

    Ajuste no menu de pausa

--- scenes/pause.lua
+++ scenes/pause.lua
@@ -14,32 +14,55 @@ function scene:create()
 	
 	local resumeButton = widget.newButton({
 		x = _W * .5,
-		y = _H * .25,
-		width = _W * .50,
-		height = _H * .25,
+		y = _H * .2,
+		width = _W * .6,
+		height = _H * .2,
 		shape = "roundedRect",
 		fillColor = {default = {0, 1, 0}, over = {0, 1, 0}},
-		cornerRadius = 10,
+		cornerRadius = 15,
 		label = "RESUME",
 		labelColor = {default = {0, 0, 0}, over = {0, 0, 0}},
-		fontSize = 25,
+		fontSize = 35,
+		strokeWidth = 3,
+		strokeColor = { default={0}, over={0} },
 		font = native.systemFontBold,
 		onRelease = function()
 			composer.hideOverlay("fade", 200)
 		end
 	})
 
-	local backToMenuButton = widget.newButton({
+	local restartButton = widget.newButton({
 		x = _W * .5,
-		y = _H * .75,
-		width = _W * .50,
-		height = _H * .25,
+		y = _H * .5,
+		width = _W * .6,
+		height = _H * .2,
 		shape = "roundedRect",
 		fillColor = {default = {0, 1, 0}, over = {0, 1, 0}},
-		cornerRadius = 10,
+		cornerRadius = 15,
+		label = "RESTART",
+		labelColor = {default = {0, 0, 0}, over = {0, 0, 0}},
+		fontSize = 35,
+		strokeWidth = 3,
+		strokeColor = { default={0}, over={0} },
+		font = native.systemFontBold,
+		onRelease = function()
+			--composer.gotoScene("scenes.game")
+		end
+	})
+	
+	local backToMenuButton = widget.newButton({
+		x = _W * .5,
+		y = _H * .8,
+		width = _W * .6,
+		height = _H * .2,
+		shape = "roundedRect",
+		fillColor = {default = {0, 1, 0}, over = {0, 1, 0}},
+		cornerRadius = 15,
 		label = "BACK TO MENU",
 		labelColor = {default = {0, 0, 0}, over = {0, 0, 0}},
-		fontSize = 25,
+		fontSize = 35,
+		strokeWidth = 3,
+		strokeColor = { default={0}, over={0} },
 		font = native.systemFontBold,
 		onRelease = function()
 			composer.gotoScene("scenes.menu","slideRight",500)
@@ -48,10 +71,8 @@ function scene:create()
 	
 	sceneGroup:insert(background)
 	sceneGroup:insert(resumeButton)
+	sceneGroup:insert(restartButton)
 	sceneGroup:insert(backToMenuButton)
-end
-
-function scene:show()
 end
 
 function scene:hide(event)
@@ -64,7 +85,6 @@ function scene:hide(event)
 end
 
 scene:addEventListener("create", scene)
-scene:addEventListener("show", scene)
 scene:addEventListener("hide", scene)
 
 return scene
